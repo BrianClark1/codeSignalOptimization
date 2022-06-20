c6612d0365825bf34711b9e3a863585f7878785c
Brian Clark <briansclark1@gmail.com>
2022-06-19 20:32:52 -0400

    Uncommented Naive

--- pairSum.js
+++ pairSum.js
@@ -2,53 +2,47 @@
 //Naive Approach 
 //O(n2) Time 
 
-// function solution(a, k) {
-//     let solutionCounter = 0;
-
-//     for (let i = 0; i < a.length - 1; i++) {
-//         for (let j = i + 1; j < a.length; j++) {
-//             if ((a[i] + a[j]) % k === 0) {
-//                 solutionCounter++;
-//             }
-//         }
-//     }
-//     return solutionCounter
-// }
-
-
-Efficient approach  Utilizing a  hashmap
 function solution(a, k) {
-  // Create a frequency array to count
-  // occurrences of all remainders when
-  // divided by K
-  let freq = new Array(K);
-    freq.fill(0);
-    // console.log(A.length)
+    let solutionCounter = 0;
 
-  // Count occurrences of all remainders
-  for (let i = 0; i < A.length; i++) ++freq[A[i] % K];
+    for (let i = 0; i < a.length - 1; i++) {
+        for (let j = i + 1; j < a.length; j++) {
+            if ((a[i] + a[j]) % k === 0) {
+                solutionCounter++;
+            }
+        }
+    }
+    return solutionCounter
+}
 
-  // If both pairs are divisible by 'K'
-  let sum = freq[0] * parseInt((freq[0] - 1) / 2, 10);
 
-  // count for all i and (k-i)
-  // freq pairs
-  for (let i = 1; i <= K / 2 && i != K - i; i++) sum += freq[i] * freq[K - i];
+// Efficient approach  Utilizing a  hashmap
+function solution(a, k) {
 
-  // If K is even
-  if (K % 2 == 0)
-    sum += parseInt(
-      (freq[parseInt(K / 2, 10)] * (freq[parseInt(K / 2, 10)] - 1)) / 2,
-      10
-    );
+  let moduloArr = new Array(k);
+    moduloArr.fill(0);
+
+    for (let i = 0; i < a.length; i++) {
+        ++moduloArr[a[i] % k];
+    }
+
+  let sum = moduloArr[0] * parseInt((moduloArr[0] - 1) / 2, 10);
+
+    for (let i = 1; (i <= k / 2 && i != k - i); i++) {
+        sum += moduloArr[i] * moduloArr[k - i];
+    }
+
+    if (k % 2 == 0) {
+        sum += parseInt(
+            (moduloArr[parseInt(k / 2, 10)] * (moduloArr[parseInt(k / 2, 10)] - 1)) / 2,
+            10
+        );
+    }
   return sum;
 }
 
-// let A = [2, 2, 1, 7, 5, 3];
-
-// let K = 4;
 
 let A = [1,2,3,4,5];
-let K = 3;
+let k = 3;
 
-console.log(solution(A, K));
+console.log(solution(A, k));
