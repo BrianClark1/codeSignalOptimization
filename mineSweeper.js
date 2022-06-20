c0bbdddbd8260de19429508b911092a31da2c915
Brian Clark <briansclark1@gmail.com>
2022-06-20 13:05:28 -0400

    Added Python & Converted JS MineSweeper solution

--- mineSweeper.js
+++ mineSweeper.js
@@ -1,415 +1,173 @@
-// function mineSweeper(inputMatrix) {
-//   var newArr = [];
 
-//   for (var i = 0; i < inputMatrix.length; i++) {
-//     newArr.push([]);
-//     for (var j = 0; j < inputMatrix[i].length; j++) {
-//       var cnt = 0;
-//       if (i > 0) {
-//         if (inputMatrix[i - 1][j]) {
-//           cnt += 1;
-//         }
-//         if (inputMatrix[i - 1][j + 1]) {
-//           cnt += 1;
-//         }
-//         if (inputMatrix[i - 1][j - 1]) {
-//           cnt += 1;
-//         }
-//       }
-//       if (i < inputMatrix.length - 1) {
-//         if (inputMatrix[i + 1][j]) {
-//           cnt += 1;
-//         }
-//         if (inputMatrix[i + 1][j + 1]) {
-//           cnt += 1;
-//         }
-//         if (inputMatrix[i + 1][j - 1]) {
-//           cnt += 1;
-//         }
-//       }
-
-//       if (inputMatrix[i][j + 1]) {
-//         cnt += 1;
-//       }
-//       if (inputMatrix[i][j - 1]) {
-//         cnt += 1;
-//       }
-//       newArr[i].push(cnt);
-//     }
-//   }
-//   return newArr;
-// }
-
-// let matrix = [[false, true, true],
-//     [true, false, true],
-//     [false, false, true]]
-    
-//     console.log(mineSweeper(matrix))
-
-
-//     const updateBoard = function (board, click) {
-//       const empty = "E";
-//       const hiddenMine = "M";
-//       const revealedMine = "X";
-//       const revealedEmpty = "B";
-
-//       function valueAt(position) {
-//         return board[position[0]][position[1]];
-//       }
-
-//       function updateAt(position, value) {
-//         board[position[0]][position[1]] = value;
-//       }
-
-//       function addAdjacentEmpty(position, stack) {
-//         adjacentPositions(position).forEach((currentPosition) => {
-//           if (valueAt(currentPosition) === empty) {
-//             let numAdjacentMines = countMines(currentPosition);
-//             if (numAdjacentMines === 0) {
-//               updateAt(currentPosition, revealedEmpty);
-//             } else {
-//               updateAt(currentPosition, `${numAdjacentMines}`);
-//             }
-//             stack.push(currentPosition);
-//           }
-//         });
-//       }
-
-//       function countMines(position) {
-//         let count = 0;
-//         adjacentPositions(position).forEach((currentPosition) => {
-//           if (valueAt(currentPosition) === hiddenMine) {
-//             count++;
-//           }
-//         });
-//         return count;
-//       }
-
-//       function isInBounds(position) {
-//         return (
-//           position[0] >= 0 &&
-//           position[0] < board.length &&
-//           position[1] >= 0 &&
-//           position[1] < board[0].length
-//         );
-//       }
-
-//       function adjacentPositions(position) {
-//         // try checking isInBounds to return only legal positions for optimization
-//         const positions = [];
-//         const row = position[0];
-//         const col = position[1];
-
-//         for (let i = row - 1; i <= row + 1; i++) {
-//           for (let j = col - 1; j <= col + 1; j++) {
-//             if (isInBounds([i, j])) {
-//               positions.push([i, j]);
-//             }
-//           }
-//         }
-//         return positions;
-//       }
-
-//       const stack = [click];
-//       while (stack.length > 0) {
-//         let currentPosition = stack.pop();
-
-//         // End the game if clicked on mine
-//         if (valueAt(click) === hiddenMine) {
-//           updateAt(click, revealedMine);
-//           return board;
-//         }
-
-//         // Edge case when user just clicks on a square that has a mine next to it
-//         // it does not spread out to reveal other empty squares
-//         let numAdjacentMines = countMines(click);
-//         if (numAdjacentMines !== 0) {
-//           updateAt(click, `${numAdjacentMines}`);
-//           return board;
-//         }
-
-//         // Spread out to reveal other empty squares
-//         let currentValue = valueAt(currentPosition);
-//         if (currentValue === empty || currentValue === revealedEmpty) {
-//           addAdjacentEmpty(currentPosition, stack);
-//         }
-//       }
-
-//       return board;
-//     };
+let field, result, test;
+field = [
+  [true, false, true, true, false],
+  [true, false, false, false, false],
+  [false, false, false, false, false],
+  [true, false, false, false, false],
+];
 
 
 
+class Solution {
+  constructor(matrix, click) {
+    this.matrix = matrix;
+    this.x = click[0];
+    this.y = click[1];
+    this.rows = this.matrix.length;
+      this.cols = this.matrix[0].length;
+      let range = n => Array.from(Array(n).keys())
 
-//     var updateBoard = function(board, click) {
-//     if(!board) return board;
-    
-//     const [r, c] = click;
-    
-//     const getAdjacentSquares = (r, c) => [
-//         [r -1, c -1],
-//         [r -1, c],
-//         [r -1, c + 1],
-//         [r, c -1],
-//         [r, c + 1],
-//         [r + 1, c -1],
-//         [r + 1, c],
-//         [r + 1, c + 1],
-//     ];
-    
-//     const value = board[r][c];
-    
-//     // game over (mine)
-//     if(value === 'M') {
-//         board[r][c] = 'X';
-        
-//         return board;
-//     }
-    
-//     const getAdjacentMines = (r, c) => {
-//         let count = 0;
-        
-//         for(let next of getAdjacentSquares(r, c)) {
-//             const [r, c] = next;
-            
-//             if(r < 0 || r > board.length -1) continue;
-//             if(c < 0 || c > board[0].length -1) continue;
-            
-//             if(board[r][c] === 'M') {
-//                 count ++;
-//             }
-//         }
-        
-//         return count;
-//     }
-    
-//     if(value === 'E') {
-//         let mines = getAdjacentMines(r, c);
-        
-//         if(mines === 0) {
-//             const dfs = (r, c) => {
-//                 if(r < 0 || r > board.length -1) return;
-//                 if(c < 0 || c > board[0].length -1) return;
-                
-//                 if(board[r][c] !== 'E') return;
-                
-//                 const adjmines = getAdjacentMines(r, c);
-                
-//                 board[r][c] = adjmines === 0 ? 'B' : String(adjmines);
-                
-//                 if(adjmines > 0) return;
-                
-//                 for(let next of getAdjacentSquares(r, c)) {
-//                     const [r, c] = next;
-                    
-//                     dfs(r, c);
-//                 }
-//             }
-            
-//             dfs(r, c);
-//         } else {
-//             board[r][c] = String(mines);
-//         }
-//     }
-    
-//     return board;
-//     };
+        //let idx = 0;
+      //initalize return matrix with -1 (converted from python)
 
+    this.return_matrix = function () {
+      let returnMatrixRows = [],
+            returnMatrixCols = range(this.rows);
+      
 
+      for (let idx = 0, lengthTemp = returnMatrixCols.length; idx < lengthTemp; idx += 1) {
+        let i = returnMatrixCols[idx];
 
+        returnMatrixRows.push(
+          function () {
+            let _pj_e = [],
+              _pj_f = range(this.cols);
 
+            for (
+              let _pj_g = 0, _pj_h = _pj_f.length;
+              _pj_g < _pj_h;
+              _pj_g += 1
+            ) {
+              let i = _pj_f[_pj_g];
 
-    // var updateBoard = function (board, click) {
-    //   if (!board) return board;
-
-    //   const [r, c] = click;
-
-    //   const getAdjacentSquares = (r, c) => [
-    //     [r - 1, c - 1],
-    //     [r - 1, c],
-    //     [r - 1, c + 1],
-    //     [r, c - 1],
-    //     [r, c + 1],
-    //     [r + 1, c - 1],
-    //     [r + 1, c],
-    //     [r + 1, c + 1],
-    //   ];
-
-    //   const value = board[r][c];
-
-    //   // game over (mine)
-    //   if (value === "M") {
-    //     board[r][c] = "X";
-
-    //     return board;
-    //   }
-
-    //   const getAdjacentMines = (r, c) => {
-    //     let count = 0;
-
-    //     for (let next of getAdjacentSquares(r, c)) {
-    //       const [r, c] = next;
-
-    //       if (r < 0 || r > board.length - 1) continue;
-    //       if (c < 0 || c > board[0].length - 1) continue;
-
-    //       if (board[r][c] === "M") {
-    //         count++;
-    //       }
-    //     }
-
-    //     return count;
-    //   };
-
-    //   if (value === "E") {
-    //     let mines = getAdjacentMines(r, c);
-
-    //     if (mines === 0) {
-    //       const dfs = (r, c) => {
-    //         if (r < 0 || r > board.length - 1) return;
-    //         if (c < 0 || c > board[0].length - 1) return;
-
-    //         if (board[r][c] !== "E") return;
-
-    //         const adjmines = getAdjacentMines(r, c);
-
-    //         board[r][c] = adjmines === 0 ? "B" : String(adjmines);
-
-    //         if (adjmines > 0) return;
-
-    //         for (let next of getAdjacentSquares(r, c)) {
-    //           const [r, c] = next;
-
-    //           dfs(r, c);
-    //         }
-    //       };
-
-    //       dfs(r, c);
-    //     } else {
-    //       board[r][c] = String(mines);
-    //     }
-    //   }
-
-    //   return board;
-    // };
-
-
-
-    // const mineSweeper = function (board, click) {
-    //   const rows = board.length;
-    //   const cols = board[0].length;
-    //   dfs(click[0], click[1]);
-    //   return board;
-
-    //   function dfs(i, j) {
-    //     if (!board[i][j]) return;
-    //     if (board[i][j] === "M") {
-    //       board[i][j] = "X";
-    //       return;
-    //     }
-    //     if (board[i][j] !== "E") return;
-
-    //     const mines = checkForMine(i, j); // Check for mines
-
-    //     if (mines) {
-    //       board[i][j] = mines.toString();
-    //       return;
-    //     } else {
-    //       // If we haven't got mines, check another cells
-    //       board[i][j] = "B";
-    //       for (let x = Math.max(i - 1, 0); x < Math.min(i + 2, rows); x++) {
-    //         for (let y = Math.max(j - 1, 0); y < Math.min(j + 2, cols); y++) {
-    //           dfs(x, y);
-    //         }
-    //       }
-    //     }
-    //   }
-
-    //   function checkForMine(x, y) {
-    //     let mines = 0;
-    //     for (let i = Math.max(x - 1, 0); i < Math.min(x + 2, rows); i++) {
-    //       for (let j = Math.max(y - 1, 0); j < Math.min(y + 2, cols); j++) {
-    //         if (board[i][j] === "M") mines++;
-    //       }
-    //     }
-    //     return mines;
-    //   }
-    // };
-
-
-    // let matrix = [
-    //   [false, true, true],
-    //   [true, false, true],
-    //   [false, false, true],
-    // ];
-
-    // console.log(mineSweeper(matrix));
-
-
-
-function mineSweeper(matrix) {
-    const board = [];
-    for (let x = 0; x < matrix.length; x++) {
-        board.push([]);
-        for (let y = 0; y < matrix[0].length; y++) {
-          board[x][y] = 0;
-          //Above
-          if (matrix[x - 1] !== undefined) {
-            if (matrix[x - 1][y]) {
-              board[x][y]++;
+              _pj_e.push(-1);
             }
-          }
-          //Below
-          if (matrix[x + 1] !== undefined) {
-            if (matrix[x + 1][y]) {
-              board[x][y]++;
+
+            return _pj_e;
+          }.call(this)
+        );
+      }
+
+      return returnMatrixRows;
+      }.call(this);
+      
+    this.visited = function () {
+      let returnMatrixRows = [],
+        returnMatrixCols = range(this.rows);
+
+      for (let idx = 0, lengthTemp = returnMatrixCols.length; idx < lengthTemp; idx += 1) {
+        let i = returnMatrixCols[idx];
+
+        returnMatrixRows.push(
+          function () {
+            let _pj_e = [],
+              _pj_f = range(this.cols);
+
+            for (
+              let _pj_g = 0, _pj_h = _pj_f.length;
+              _pj_g < _pj_h;
+              _pj_g += 1
+            ) {
+              let i = _pj_f[_pj_g];
+
+              _pj_e.push(false);
             }
-          }
-          //Left
-          if (matrix[x][y - 1] !== undefined) {
-            if (matrix[x][y - 1]) {
-              board[x][y]++;
-            }
-          }
-          //Right
-          if (matrix[x][y + 1] !== undefined) {
-            if (matrix[x][y + 1]) {
-              board[x][y]++;
-            }
-          }
-          //Top Left
-          if (matrix[x - 1] !== undefined) {
-            if (matrix[x - 1][y - 1]) {
-              board[x][y]++;
-            }
-          }
-          //Top Right
-          if (matrix[x - 1] !== undefined) {
-            if (matrix[x - 1][y + 1]) {
-              board[x][y]++;
-            }
-          }
-          //Bottom Left
-          if (matrix[x + 1] !== undefined) {
-            if (matrix[x + 1][y - 1]) {
-              board[x][y]++;
-            }
-          }
-          //Bottom Right
-          if (matrix[x + 1] !== undefined) {
-            if (matrix[x + 1][y + 1]) {
-              board[x][y]++;
-            }
-          }
-        }
-    }
-    return board;
-}
-    
-    let matrix = [
-      [false, true, true],
-      [true, false, true],
-      [false, false, true],
+
+            return _pj_e;
+          }.call(this)
+        );
+      }
+
+      return returnMatrixRows;
+    }.call(this);
+  }
+
+  get_adjacent_squares(x, y) {
+    let adj;
+    adj = [
+      [x - 1, y - 1],
+      [x, y - 1],
+      [x + 1, y - 1],
+      [x - 1, y],
+      [x + 1, y],
+      [x - 1, y + 1],
+      [x, y + 1],
+      [x + 1, y + 1],
     ];
+    return adj;
+  }
 
-    console.log(mineSweeper(matrix));
+  inBoard(x, y) {
+    return x >= 0 && x < this.rows && y >= 0 && y < this.cols;
+  }
+
+  countMines(x, y) {
+    let count;
+    count = 0;
+
+    for (
+      let square,
+        idx = 0,
+        returnMatrixRows = this.get_adjacent_squares(x, y),
+        returnMatrixCols = returnMatrixRows.length;
+      idx < returnMatrixCols;
+      idx += 1
+    ) {
+      square = returnMatrixRows[idx];
+
+      if (
+        this.inBoard(square[0], square[1]) &&
+        this.matrix[square[0]][square[1]] === true
+      ) {
+        count += 1;
+      }
+    }
+
+    return count;
+  }
+
+  reveal(x, y) {
+    let adj, n;
+    this.visited[x][y] = true;
+    n = this.countMines(x, y);
+
+    if (n !== 0) {
+      this.return_matrix[x][y] = n;
+      return;
+    } else {
+      this.return_matrix[x][y] = 0;
+      adj = this.get_adjacent_squares(x, y);
+
+      for (
+        let square, idx = 0, returnMatrixRows = adj, returnMatrixCols = returnMatrixRows.length;
+        idx < returnMatrixCols;
+        idx += 1
+      ) {
+        square = returnMatrixRows[idx];
+
+        if (
+          this.inBoard(square[0], square[1]) &&
+          !this.visited[square[0]][square[1]]
+        ) {
+          this.reveal(square[0], square[1]);
+        }
+      }
+    }
+
+    return;
+  }
+
+  play(x, y) {
+    this.reveal(x, y);
+    return this.return_matrix;
+  }
+}
+
+test = new Solution(field, [3,2]);
+result = test.play(3, 2);
+console.log(result);
+
+
+
+
---
+++ mineSweeper.py
@@ -0,0 +1,75 @@
+field = [
+  [True, False, True, True, False],
+  [True, False, False, False, False],
+  [False, False, False, False, False],
+  [True, False, False, False, False],
+]
+
+class Solution:
+    # Constructoor, initializes input matrix, deconstructs click, find size of rows & columns 
+    #Creates return matrix, initialized with -1's along with validation matrix filled with False's
+    def __init__(self,matrix,click):
+        self.matrix = matrix  # [[T F T T F]].... True = bomb, False = nobomb
+        self.x = click[0]
+        self.y = click[1]
+        self.rows = len(self.matrix)
+        self.cols = len(self.matrix[0])
+        self.return_matrix = [[-1 for i in range(self.cols)] for i in range(self.rows)]
+        self.visited = [[False for i in range(self.cols)] for i in range(self.rows)] 
+        
+        # All 8 possible adjacent squares and their corresponding coordinates 
+    def get_adjacent_squares(self,x,y):
+        adj = [[x-1,y-1],
+               [x,y-1],
+               [x+1,y-1],
+               [x-1,y],
+               [x+1,y],
+               [x-1,y+1],
+               [x,y+1],
+               [x+1,y+1]
+            ]
+        return adj
+       
+       # Checks whether the coordinate or square is valid (in bounds)
+    def inBoard(self,x,y):
+        return x>=0 and x<self.rows and y>=0 and y<self.cols
+    
+    #loops through adjacent squares and checks whethere there is a mine there with the given coordinates
+    #if there is a mine , increment the count to eventually popualte the return matrix with 
+    def countMines(self,x,y):
+        count = 0
+        for square in self.get_adjacent_squares(x,y):
+            if self.inBoard(square[0],square[1]) and self.matrix[square[0]][square[1]]==True:
+                count +=1
+        return count
+            
+    # If there are adjacent mines, populate the position in our return matrix with its corresponding digit 
+    #Mark that the coordinate has been visited in our visitation matrix    
+    def reveal(self,x,y):
+        self.visited[x][y] = True
+        n = self.countMines(x,y)
+        if n!=0:
+            self.return_matrix[x][y]=n
+            return
+        
+        # If n is 0, then there are no adjacent mines, begin to propagate and check adjacent mines. Repeat process if n is 0 (no adjacent mines)
+        #Recursive call , propagation. Does most of the leg work 
+        #Checks whethere its inside the board, valid positioning and whether its been visited before 
+        #If both are true, run reveal method and either populate with posotive > 0 digit or continue to propagate
+        else:
+            self.return_matrix[x][y]=0
+            
+            adj = self.get_adjacent_squares(x, y)
+            
+            for square in adj:
+                if self.inBoard(square[0],square[1]) and not self.visited[square[0]][square[1]]:
+                    self.reveal(square[0],square[1])
+        return
+    
+    def play(self,x,y):
+        self.reveal(x,y)
+        return self.return_matrix
+                         
+test = Solution(field,click)
+result = test.play(3,2)
+print(result)
